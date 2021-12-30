dca1bea3821c7287716ecdb036cd7e13c27ee65d
kyle <keginzburg@gmail.com>
2021-12-30 15:22:28 -0800

    Did minor font and logo styling for splash page

--- frontend/components/splash/login_header.jsx
+++ frontend/components/splash/login_header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const LoginHeader = () => {
   return (
     <div className="login-header-container">
-      <img src="https://fontmeme.com/temporary/41cf7cbb25db50b3ef62ace2f2463698.png" alt="Webflix Logo" />
+      <img src="https://fontmeme.com/permalink/211230/41cf7cbb25db50b3ef62ace2f2463698.png" alt="netflix-font" border="0" />
       <Link to="/login">Sign In</Link>
     </div>
   )
--- frontend/components/splash/splash_page.jsx
+++ frontend/components/splash/splash_page.jsx
@@ -1,19 +1,57 @@
 import React from 'react';
+import { Redirect } from 'react-router';
 import LoginHeader from './login_header';
 
-const SplashPage = () => {
-  return (
-    <div className='splash-container'>
-      <LoginHeader />
-      <p>
-        Sign Up
-      </p>
-      <footer>
-        Personal Links
-      </footer>
-    </div>
+class SplashPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: ""
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-  )
+  update(field) {
+    return (e) => {
+      this.setState({ [field]: e.currentTarget.value })
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    <Redirect to="/signup" email={this.state.email} />
+  }
+
+  render() {
+    return (
+      <div className='splash-container'>
+        <LoginHeader />
+        <div className="tagline-signup">
+          <h2>Unlimited web related movies and more.</h2>
+          <h3>Watch anywhere. Cancel anytime.</h3>
+          <h4>Ready to watch? Enter your email to create or restart your membership.</h4>
+          <form onSubmit={this.handleSubmit}>
+            <label>Email address
+              <input type="text" value={this.state.email} onChange={this.update('email')} />
+              <button type="submit">Get Started ></button>
+            </label>
+
+          </form>
+        </div>
+        <div>
+          
+        </div>
+        <footer>
+          <p>Questions? Call 305-962-6631</p>
+
+          <a href="https://github.com/keginzburg/webflix">GitHub</a>
+          <a href="#">My AngelList</a>
+          <a href="https://www.linkedin.com/in/kyleginzburg/">My LinkedIn</a>
+        </footer>
+      </div>
+
+    )
+  }
 }
 
 export default SplashPage;
