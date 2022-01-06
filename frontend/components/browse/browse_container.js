db6eb5a2a4eb87190d17f61332c231617d5acc31
kyle <keginzburg@gmail.com>
2022-01-06 14:26:59 -0500

    Added logoutCurrentUser session action creator for hard logout

--- frontend/components/browse/browse_container.js
+++ frontend/components/browse/browse_container.js
@@ -1,6 +1,7 @@
 import { connect } from "react-redux";
 import { logout } from "../../actions/session_actions";
 import { fetchUserProfiles } from "../../actions/profile_actions";
+import { logoutCurrentUser } from "../../actions/session_actions";
 import Browse from "./browse";
 
 const mapStateToProps = (state, ownProps) => ({
@@ -12,6 +13,7 @@ const mapStateToProps = (state, ownProps) => ({
 const mapDispatchToProps = dispatch => ({
   logout: () => dispatch(logout()),
   fetchUserProfiles: userId => dispatch(fetchUserProfiles(userId)),
+  logoutCurrentUser: () => dispatch(logoutCurrentUser()),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Browse);
