673b53ee9e7709d457758f59a041032c910f73e0
kyle <keginzburg@gmail.com>
2022-01-06 13:34:29 -0500

    Accepted incoming change from profiles in splash container which was an added comma

--- frontend/components/splash/splash_page_container.js
+++ frontend/components/splash/splash_page_container.js
@@ -8,11 +8,7 @@ const mapStateToProps = (state, ownProps) => ({
 
 const mapDispatchToProps = dispatch => ({
   receiveNewEmail: newEmail => dispatch(receiveNewEmail(newEmail)),
-<<<<<<< HEAD
-  login: user => dispatch(login(user)),
-=======
   login: user => dispatch(login(user))
->>>>>>> profiles
 });
 
 export default connect(null, mapDispatchToProps)(SplashPage);
